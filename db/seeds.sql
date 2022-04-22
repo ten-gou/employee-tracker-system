b40f8dc7f6a0e72c31e360b4ef1768931ac66dbe
Maribel <lwang79@zoho.com>
2022-04-22 11:02:50 -0700

    added the base for the seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+/*Departments Seed - 1 value to input*/
+INSERT INTO departments (department_name)
+VALUES
+    ()
+    ()
+
+/*Roles Seed - 4 values to input*/
+INSERT INTO ROLES (job_title, job_id, job_salary, department_id)
+VALUES
+    ()
+    ()
+
+/*Employees Seed - 6 values to input*/
+INSERT INTO employees (employee_first_name, employee_last_name, employee_job_title, employee_department_id, employee_salary, employee_managers)
+VALUES
+    ()
+    ()
